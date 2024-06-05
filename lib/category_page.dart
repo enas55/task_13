f492e13a48e70ed63b94634d9823746082784f1a
enastaher55 <enastaher06@gmail.com>
2024-06-05 23:04:44 +0300

    fixing more errors

--- lib/category_page.dart
+++ lib/category_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:task_13/add_items.dart';
@@ -19,19 +21,33 @@ class _CategoryPageState extends State<CategoryPage> {
   @override
   void initState() {
     getData();
-    getInfo ();
+    getInfo();
     super.initState();
   }
 
   Future<void> getData() async {
-  GetIt.I.get<SqlHelper>().createTables();
+    GetIt.I.get<SqlHelper>().createTables();
   }
 
-  Future<void> getInfo () async{
-    var sqlHelper = GetIt.I.get<SqlHelper>();
-    await sqlHelper.db!.query('Categories');
-    setState(() {
-    });
+  Future<void> getInfo() async {
+    try {
+      var sqlHelper = GetIt.I.get<SqlHelper>();
+      final results = await sqlHelper.db!.query('Categories');
+      List<CategoryData> cat = [];
+      for (var item in results) {
+        cat.add(
+          CategoryData.fromJson(
+            {
+              'name': item['name'],
+              'description': item['description'],
+            },
+          ),
+        );
+      }
+      setState(() {});
+    } catch (e) {
+      log("Error : $e");
+    }
   }
 
   List<CategoryData> cat = [];
@@ -39,38 +55,40 @@ class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 5, 98, 175),
-        title: const Text(
-          'Categories',
-          style: TextStyle(color: Colors.white),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (ctx) => const AddItems(),
-                ),
-              );
-            },
-            icon: const Icon(
-              Icons.add,
-              color: Colors.white,
-            ),
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 5, 98, 175),
+          title: const Text(
+            'Categories',
+            style: TextStyle(color: Colors.white),
           ),
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: cat.length,
-        itemBuilder: (context, index) {
-        var result = cat[index];
-        return ListTile(
-          title: Text(result.name ?? 'No name found'),
-          subtitle: Text(result.description ?? 'No description found'),
-        );
-      })
-    );
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (ctx) => const AddItems(),
+                  ),
+                );
+              },
+              icon: const Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        body: cat.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: cat.length,
+                itemBuilder: (context, index) {
+                  var result = cat[index];
+                  return ListTile(
+                    title: Text(result.name ?? 'No name found'),
+                    subtitle:
+                        Text(result.description ?? 'No description found'),
+                  );
+                }));
   }
 }
